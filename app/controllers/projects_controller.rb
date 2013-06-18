db1ebad34e4e73419a503ae006abdab915c3d44a
Jamie Johnson <jajohnson822007@gmail.com>
2013-06-18 09:13:41 -0400

    Set up gem dependencies and run rspec generator

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,24 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def new
+
+    @project = Project.new
+  end
+
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been created."
+      render :action => "new"
+    end
+  end
+
+
+
+end
